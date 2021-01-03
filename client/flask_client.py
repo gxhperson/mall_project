a29bdd9e137523aafe4c58bf9fb9c6c66e7a7126
Lrq <2079925077@qq.com>
2021-01-03 18:31:34 +0800

    my

---
+++ client/flask_client.py
@@ -0,0 +1,69 @@
+######################################################
+#        > File Name: flask_client.py
+#      > Author: GuoXiaoNao
+ #     > Mail: 250919354@qq.com 
+ #     > Created Time: Mon 20 May 2019 11:52:00 AM CST
+ ######################################################
+
+from flask import Flask, send_file
+import sys
+
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    #首页
+    return send_file('templates/index.html')
+
+@app.route('/login')
+def login():
+    #登录
+    return send_file('templates/login.html')
+
+@app.route('/register')
+def register():
+    #注册
+    return send_file('templates/register.html')
+
+@app.route('/forget_password')
+def forget_password(username):
+    #个人信息
+    return send_file('templates/forget_password.html')
+
+@app.route('/info_center')
+def info_center(username):
+    #修改个人信息
+    return send_file('templates/info_center.html')
+
+@app.route('/product_info')
+def product_info(username):
+    #修改密码
+    return send_file('templates/product_info.html')
+
+
+@app.route('/search_product')
+def search_product(username):
+    #发表博客
+    return send_file('templates/search_product.html')
+
+
+@app.route('/shopping_cart')
+def shopping_cart(username):
+    #个人博客列表
+    return send_file('templates/shopping_cart.html')
+
+# @app.route('/<username>/topics/detail/<t_id>')
+# def topics_detail(username, t_id):
+#     #博客内容详情
+#     return send_file('templates/detail.html')
+#
+
+# @app.route('/test_api')
+# def test_api():
+#     #测试
+#     return send_file('templates/test_api.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
